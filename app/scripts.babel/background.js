caf5f569f784209703067991cb5d9e5eb7a293f4
Dacrol <jonas.ahrens@gmail.com>
2017-07-07 22:23:37 +0200

    Add popout window

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var tabView;
+
 chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
@@ -7,3 +9,20 @@ chrome.runtime.onInstalled.addListener(details => {
 chrome.browserAction.setBadgeText({text: '\'Allo'});
 
 console.log('\'Allo \'Allo! Event Page for Browser Action');
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  console.log(tabView);
+  openTabView();
+});
+
+function openTabView() {
+  if (tabView) {
+    chrome.windows.update(tabView, {focused: true});
+  } else {
+    chrome.windows.create(
+        {'url': 'tabview.html', 'width': 400, 'height': 600, 'type': 'popup', 'focused': true},
+        function(chromeWindow) {
+            tabView = chromeWindow.id;
+        })
+  }
+}
